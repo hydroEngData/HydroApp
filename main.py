accbd61e433599635218b072c5b31c90e7be6688
hydroEngData <arcticwalker@gmail.com>
2022-10-28 07:51:29 +0200

    valid datitme12

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if add_selectbox == "Data organiser tool":
     data['sel_date'] = data[time_col]
     data['sel_date'] = data['sel_date'].dt.strftime('%y-%m-%d %h:%I:%s')
     #data['sel_date'] = data['sel_date'].astype('datetime64[ns]')
-    data['sel_date'] = pd.to_datetime(data['sel_date'])
+    #data['sel_date'] = pd.to_datetime(data['sel_date'])
     #df = data.select_dtypes(include=[np.datetime64])
 
     # if data['sel_date'].dtype == 'object':
